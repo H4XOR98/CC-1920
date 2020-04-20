2570e2faaabd66aab3b6db013fe7ea2f22edd217
Gonçalo Almeida <48161282+Gonkalos@users.noreply.github.com>
2020-04-20 01:17:52 +0100

    Add files via upload

---
+++ TP2/src/AnonGWCloud.java
@@ -0,0 +1,64 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class AnonGWCloud {
+    private Map<String,Integer> clients;
+    private Map<Integer,String> clientsRequests;
+    private Map<Integer,byte[]> serversReplys;
+    private Queue<Integer> requestsQueue;
+
+    private static int requestId = 0;
+    private final String serverAddress;
+    private final String localAnonGWAddress;
+
+    public AnonGWCloud(String serverAddress, String localAnonGWAddress){
+        this.clients = new HashMap<>();
+        this.clientsRequests = new HashMap<>();
+        this.serversReplys = new HashMap<>();
+        this.requestsQueue = new PriorityQueue<>();
+        this.serverAddress = serverAddress;
+        this.localAnonGWAddress = localAnonGWAddress;
+    }
+
+    public synchronized void insertRequest(String clientAddress, String request){
+        if(clientAddress != null && request != null && !clients.containsKey(clientAddress)){
+            this.clients.put(clientAddress,requestId);
+            this.clientsRequests.put(requestId,request.replace(serverAddress, localAnonGWAddress));
+            requestsQueue.add(requestId);
+            requestId++;
+        }
+    }
+
+    public synchronized void insertReply(int id, byte[] file){
+        if(clients.containsKey(id) && file != null){
+            this.serversReplys.put(requestId,file);
+        }
+    }
+
+    public synchronized Pair<Integer,String> getRequest(){
+        Pair<Integer,String> request = null;
+        if(this.requestsQueue.size() > 0){
+            int id = this.requestsQueue.poll();
+            if(this.clientsRequests.containsKey(id)){
+                request = new Pair(id,this.clientsRequests.get(id));
+                this.clientsRequests.remove(id);
+            }
+        }
+        return request;
+    }
+
+    public synchronized byte[] getReply(String clientAddress){
+        byte[] file = null;
+
+        if(this.clients.containsKey(clientAddress)){
+            int id = this.clients.get(clientAddress);
+            if(this.serversReplys.containsKey(id)){
+                file = this.serversReplys.get(id);
+                this.serversReplys.remove(id);
+            }
+        }
+        return file;
+    }
+}
---
+++ TP2/src/AnonGWMain.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnonGWMain {
+    public static void main(String[] args) throws IOException {
+        if (args.length < 6) {
+            throw new IllegalArgumentException("insuficient arguments");
+        }
+
+        String serverIP = args[1];
+        int port = Integer.parseInt(args[3]);
+
+        List<String> peers;
+        peers = new ArrayList<>();
+        for(int i = 6 ; i < args.length ; i++){
+            peers.add(args[i]);
+        }
+
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        String localAnonGWIP = inetAddress.getHostAddress();
+
+        AnonGWCloud cloud = new AnonGWCloud(serverIP, localAnonGWIP);
+
+        InetAddress localAddress = InetAddress.getByName(serverIP);
+        ServerSocket s = new ServerSocket(port);
+        System.out.println("Socket server open");
+
+        Socket socketServer = new Socket(localAddress, port);
+        new Thread(new ServerTask(cloud,socketServer)).start();
+
+        while (true) {
+            Socket socketClient = s.accept();
+            System.out.println("Socket cliente open");
+            Thread readerClient = new Thread(new AnonGwReader(cloud, socketClient));
+            Thread writerClient = new Thread(new AnonGwWriter(cloud, socketClient));
+            readerClient.start();
+            writerClient.start();
+        }
+    }
+}
